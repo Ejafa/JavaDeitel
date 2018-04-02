0fb021a5f4df9c2dd9cd1f1bf41baf0c762801b5
Marco Ramirez <chidochidochido@gmail.com>
2018-04-02 16:55:58 -0500

    Exercise 6.15

--- JavaDeitel/Chapter6/Deitel_6_15/src/com/company/Main.java
+++ JavaDeitel/Chapter6/Deitel_6_15/src/com/company/Main.java
@@ -1,8 +1,21 @@
 package com.company;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
 	// write your code here
+        Scanner myScanner = new Scanner(System.in);
+        int side1, side2;
+        double hypothenuse;
+
+        System.out.printf("Enter side 1: ");
+        side1 = myScanner.nextInt();
+        System.out.printf("Enter side 2: ");
+        side2 = myScanner.nextInt();
+
+        hypothenuse = Math.sqrt(Math.pow(side1, 2) + Math.pow(side2, 2));
+        System.out.printf("Hypothenuse: %f", hypothenuse);
     }
 }
